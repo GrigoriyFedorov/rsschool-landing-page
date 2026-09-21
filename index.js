531711d2e2bad913782eefbca82df2aefaed0af2
Grigoriy <grisha.2004.aaa@gmail.com>
2026-09-21 12:23:08 +0300

    feat: add hero section

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const button = document.querySelector('.theme-switch');
+const button = document.querySelectorAll(".theme-switch");
 const html = document.documentElement;
 
-button.addEventListener('click', () => {
-  if (html.hasAttribute('data-theme')) {
-    html.removeAttribute('data-theme');
-    localStorage.setItem('theme', 'light');
-  } else {
-    html.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  }
-})
+button.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (html.hasAttribute("data-theme")) {
+      html.removeAttribute("data-theme");
+      localStorage.setItem("theme", "light");
+    } else {
+      html.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    }
+  });
+});
 
-if (localStorage.getItem('theme') === 'dark') {
-  html.setAttribute('data-theme', 'dark');
+if (localStorage.getItem("theme") === "dark") {
+  html.setAttribute("data-theme", "dark");
 }
